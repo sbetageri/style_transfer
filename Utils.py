de36c7fc9841ab1c1f5a6fdcc587dac595ee3db0
Srirakshith Betageri <sbetageri111@gmail.com>
2018-12-15 22:21:57 -0600

    Get feature maps from VGG

--- Utils.py
+++ Utils.py
@@ -1,4 +1,5 @@
-from torchvision import models
+from torchvision import models, transforms
+from PIL import Image
 
 def get_model():
     '''Obtains the features of a pretrained model.
@@ -7,3 +8,16 @@ def get_model():
     '''
     model = models.vgg11(pretrained=True)
     return model.features
+
+def load_image(path):
+    image = Image.open(path).convert('RGB')
+    
+    img_transform = transforms.Compose([
+                        transforms.Resize(640),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.485, 0.456, 0.406), 
+                                             (0.229, 0.224, 0.225))])
+    image = img_transform(image)
+    image = image[:3,:,:].unsqueeze(0)
+    
+    return image
